beb01940ac32f4b5b8fb376f659273ceed2d1262
郭健 <guojian_k@qq.com>
2018-05-22 00:32:59 +0800

    添加地图

--- core/src/com/mygdx/game/MarioBros.java
+++ core/src/com/mygdx/game/MarioBros.java
@@ -1,29 +1,22 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.screen.PlayScreen;
 
 public class MarioBros extends Game {
-    public static final int V_WIDTH=400;
-    public static final int V_HEIGHT=208;
-	public SpriteBatch batch;
+    public static final float V_WIDTH=400f;
+    public static final float V_HEIGHT=208f;
+	public static final SpriteBatch batch = new SpriteBatch();
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
 	    super.render();
-//		Gdx.gl.glClearColor(1, 0, 0, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		batch.end();
 	}
 	
 	@Override
--- core/src/com/mygdx/game/screen/PlayScreen.java
+++ core/src/com/mygdx/game/screen/PlayScreen.java
@@ -1,27 +1,59 @@
 package com.mygdx.game.screen;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MarioBros;
 import com.mygdx.game.scenes.Hub;
 
 
 public class PlayScreen implements Screen {
     private MarioBros game;
-//    OrthographicCamera gamecam;
-//    Viewport gamePort;
+    OrthographicCamera gamecam;
+    Viewport gamePort;
     Hub hub;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
-//        gamecam = new OrthographicCamera();
-//        gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gamecam);
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
         hub = new Hub(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("world.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(MarioBros.V_WIDTH/2,MarioBros.V_HEIGHT/2,0);
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        gamecam.update();
+        mapRenderer.setView(gamecam);
+    }
+
+    private void handleInput(float dt) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            gamecam.position.x += 100 * dt;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            gamecam.position.x -= 100 * dt;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            gamecam.position.y += 100 * dt;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            gamecam.position.y -= 100 * dt;
+        }
+
     }
 
     @Override
@@ -31,20 +63,21 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0,0,0,1);
+        update(delta);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        mapRenderer.render();
         game.batch.setProjectionMatrix(hub.stage.getCamera().combined);
         hub.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-//        gamePort.update(width,height);
+        gamePort.update(width, height);
     }
 
     @Override
     public void pause() {
-
     }
 
     @Override
